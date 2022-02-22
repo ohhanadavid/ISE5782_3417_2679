c1ae0e1cfe27501c43e8709f4b90bf2c5c1cb1c7
ohhanadavid <sustujbv167@gmail.com>
2022-02-22 19:22:36 +0200

    part one

--- src/src/primitives/Vector.java
+++ src/src/primitives/Vector.java
@@ -3,8 +3,16 @@ package src.primitives;
 public class Vector extends Point {
 
    public Vector (Point point){
-      super(point.xyz.d1,point.xyz.d2,point.xyz.d3);
+
+       super(point.xyz.d1,point.xyz.d2,point.xyz.d3);
    }
+   public Vector (Double3 double3){
+       super(double3);
+   }
+    public Vector(double d1,double d2, double d3) {
+        super(d1,d2,d3);
+
+    }
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
@@ -12,20 +20,15 @@ public class Vector extends Point {
 
     @Override
     public String toString() {
-        return "Vector{}";
+        return "Vector:"+super.toString();
     }
 
     public Vector add(Vector vector){
         return new Vector( super.add(vector));
     }
 
-    public Vector scale(double scale){
-        return new Vector(this.xyz.d1*scale,this.xyz.d2*scale,this.xyz.d3*scale);
-    }
-
-    public Vector(double d1,double d2, double d3) {
-        super(d1,d2,d3);
-
+    public Vector scale (double scale) {
+        return new Vector(xyz.scale(scale));
     }
 
     public Vector crossProduct(Vector edge2) {
@@ -41,6 +44,23 @@ public class Vector extends Point {
                 this.xyz.d3*vector.xyz.d3;
     }
 
+    public double length(){
+       return this.xyz.d1*this.xyz.d1+this.xyz.d2*this.xyz.d2+this.xyz.d3*this.xyz.d3;
+    }
+
+    public double lengthSquared(){
+       return Math.sqrt(this.length());
+    }
+
+    public Vector normalize(){
+       var len=lengthSquared();
+       return  new Vector(this.xyz.d1/len,this.xyz.d2/len,this.xyz.d3/len);
+    }
+
+    public double dotProdact(Vector vector){
+       var prodact=this.xyz.product(vector.xyz);
+       return prodact.d1+prodact.d3+prodact.d2;
+    }
 
 
 }
