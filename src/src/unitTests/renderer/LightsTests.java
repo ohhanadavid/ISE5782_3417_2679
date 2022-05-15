0339dac16b910462e828048eedb8353141601e86
Aviad <Aviad.kle@gmail.com>
2022-05-15 23:45:33 +0300

    Pr07

--- src/src/unitTests/renderer/LightsTests.java
+++ src/src/unitTests/renderer/LightsTests.java
@@ -185,4 +185,60 @@ public class LightsTests {
 				.writeToImage(); //
 
 	}
+	/**
+	 * Produce a picture of a two triangles lighted by a multiple lights
+	 */
+	@Test
+	public void TrianglesMultiLight() {
+		scene2.geometries.add(triangle1.setMaterial(new Material().setKd(0.8).setKs(0.2).setShininess(300)), //
+				triangle2.setMaterial(new Material().setKd(0.8).setKs(0.2).setShininess(300)));
+		//direction light
+		scene2.lights.add(new DirectionalLight(new Color(400, 30, 0), new Vector(0.65, 0, -1)));
+		//spot light
+		scene2.lights.add(new SpotLight(new Color(500, 250, 250), new Vector(-150100, -1000, 1), new Vector(new Point(10, -10, -130))) //
+				.setKl(0.0001).setKq(0.000005));
+		//point light
+		scene2.lights.add(new PointLight(new Color(500, 250, 250), new Point(110, -10, -130)) //
+				.setKl(0.0005).setKq(0.0005));
+
+
+		ImageWriter imageWriter = new ImageWriter("MultiLightTriangles", 500, 500);
+		camera2.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene2)) //
+				.renderImage()
+				.writeToImage(); //
+	}
+
+	/**
+	 * Produce a picture of a sphere lighted by a improve spot light
+	 */
+	@Test
+	public void sphereImproveSpotLights() {
+		scene1.geometries.add(sphere);
+		//spot light
+		scene1.lights.add(new SpotLight(new Color(600, 0, 0), new Vector(1, 1, -2), new Vector(new Point(-200, 100, 50))).setSpecularN(30) //
+				.setKl(0.00000005).setKq(0.000000005));
+
+		ImageWriter imageWriter = new ImageWriter("ImproveSpotLightSphere", 500, 500);
+		camera1.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene1)) //
+				.renderImage()
+				.writeToImage(); //
+
+	}
+
+	@Test
+	public void ImproveTrianglesLight() {
+		scene2.geometries.add(triangle1.setMaterial(new Material().setKd(0.5).setKs(0.6).setShininess(10)));
+
+		scene2.lights.add(new SpotLight(new Color(600, 0, 250), new Vector(-2, -2, -1), new Vector(new Point(10, -12, -130))).setSpecularN(100) //
+				.setKl(0.0001).setKq(0.000005));
+
+		ImageWriter imageWriter = new ImageWriter("ImproveLightTriangles", 500, 500);
+		camera2.setImageWriter(imageWriter) //
+				.setRayTracer(new RayTracerBasic(scene2)) //
+				.renderImage()
+				.writeToImage(); //
+	}
+
 }
